a29811b8af3d23a4a7c31792c556a3c900846df0
WEBEEQ-DESKTOP\Robert <webeeq@gmail.com>
2024-04-23 20:06:16 +0200

    Update repository files

--- migrations/Version20240423175923.php
+++ migrations/Version20240423175923.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240423172344 extends AbstractMigration
+final class Version20240423175923 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -22,7 +22,7 @@ final class Version20240423172344 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE city (id SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL, province_id SMALLINT UNSIGNED DEFAULT NULL, name VARCHAR(30) NOT NULL, INDEX IDX_2D5B0234E946114A (province_id), INDEX name (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE province (id SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL, name VARCHAR(30) NOT NULL, INDEX name (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE user (id INT UNSIGNED AUTO_INCREMENT NOT NULL, province_id SMALLINT UNSIGNED DEFAULT NULL, city_id SMALLINT UNSIGNED DEFAULT NULL, name VARCHAR(30) NOT NULL, surname VARCHAR(50) NOT NULL, description LONGTEXT NOT NULL, ranking INT UNSIGNED NOT NULL, number INT UNSIGNED NOT NULL, ip VARCHAR(15) NOT NULL, date DATETIME NOT NULL, INDEX IDX_8D93D649E946114A (province_id), INDEX IDX_8D93D6498BAC62AF (city_id), INDEX name (name), INDEX surname (surname), INDEX ranking (ranking), INDEX number (number), INDEX date (date), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT UNSIGNED AUTO_INCREMENT NOT NULL, province_id SMALLINT UNSIGNED DEFAULT NULL, city_id SMALLINT UNSIGNED DEFAULT NULL, name VARCHAR(30) NOT NULL, surname VARCHAR(50) NOT NULL, description TEXT NOT NULL, ranking INT UNSIGNED NOT NULL, number INT UNSIGNED NOT NULL, ip VARCHAR(15) NOT NULL, date DATETIME NOT NULL, INDEX IDX_8D93D649E946114A (province_id), INDEX IDX_8D93D6498BAC62AF (city_id), INDEX name (name), INDEX surname (surname), INDEX ranking (ranking), INDEX number (number), INDEX date (date), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE city ADD CONSTRAINT FK_2D5B0234E946114A FOREIGN KEY (province_id) REFERENCES province (id)');
         $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D649E946114A FOREIGN KEY (province_id) REFERENCES province (id)');
